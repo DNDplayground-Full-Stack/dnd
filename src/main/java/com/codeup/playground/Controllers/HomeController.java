03f6c8f8414e2940669a7fbdbfae7b2ec7358faa
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-06-08 20:27:04 -0500

    connecting sql and connected views

--- src/main/java/com/codeup/playground/Controllers/HomeController.java
+++ src/main/java/com/codeup/playground/Controllers/HomeController.java
@@ -2,12 +2,16 @@ package com.codeup.playground.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String hello() {
         return "Landing";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "Login";
     }
 }
