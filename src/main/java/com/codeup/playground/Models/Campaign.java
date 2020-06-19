974cedba84c2b8a5bce5268d78314ac61c656b5b
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-06-18 23:21:51 -0500

    setting up tables...

--- src/main/java/com/codeup/playground/Models/Campaign.java
+++ src/main/java/com/codeup/playground/Models/Campaign.java
@@ -1,24 +1,24 @@
-package com.codeup.playground.Models;
-
-
-import javax.persistence.*;
-
-@Entity
-@Table(name="campaigns")
-public class Campaign {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-    private long id;
-
-    @Column(length = 200, nullable = false)
-    private String name;
-
-    @ManyToOne
-    @JoinColumn(name="DM")
-    private User user;
-
-    @Column(nullable = false, columnDefinition="text")
-    private String Description;
-}
+//package com.codeup.playground.Models;
+//
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name="campaigns")
+//public class Campaign {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+//    private long id;
+//
+//    @Column(length = 200, nullable = false)
+//    private String name;
+//
+//    @ManyToOne
+//    @JoinColumn(name="DM")
+//    private User user;
+//
+//    @Column(nullable = false, columnDefinition="text")
+//    private String Description;
+//}
