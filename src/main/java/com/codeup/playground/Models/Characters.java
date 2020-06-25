b036d0312e20c9b8b00fc3adf6c8551e3d8204d0
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-06-24 21:16:34 -0500

    updating character table

--- src/main/java/com/codeup/playground/Models/Characters.java
+++ src/main/java/com/codeup/playground/Models/Characters.java
@@ -1,6 +1,7 @@
 package com.codeup.playground.Models;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "characters")
@@ -13,53 +14,52 @@ public class Characters {
     @Column(nullable = false)
     private String name;
 
-    //    @Column(length =25, nullable = false)
-//    private String race;
-//
-//    @Column(length =25, nullable = false)
-//    private String playerClass;
-//
-//    @Column(length =25, nullable = false)
-//    private String level;
-//
+    @Column(nullable = false)
+    private String race;
+
+    @Column(nullable = false)
+    private String playerClass;
+
+    @Column(nullable = false)
+    private String level;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-//    @ManyToOne
-//    @JoinColumn(name = "campaign")
-//    private Campaign campaign;
+    @ManyToOne
+    @JoinColumn(name = "campaign")
+    private Campaign campaign;
 
-//    @Column(length =25, nullable = false)
-//    private String background;
-//
-//    @Column(length =25, nullable = false)
-//    private String alignment;
-//
-//    @Column(nullable = false)
-//    private long xp;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int proficiencyBonus;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int strength;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int dexterity;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int constitution;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int intelligence;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int wisdom;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int charisma;
+    @Column(length =25, nullable = false)
+    private String background;
+
+    @Column(length =25, nullable = false)
+    private String alignment;
+
+    @Column(nullable = false)
+    private long xp;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int proficiencyBonus;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int strength;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int dexterity;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int constitution;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int intelligence;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int wisdom;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int charisma;
 
 //    @OneToMany(cascade = CascadeType.ALL, mappedBy = "player")
 //    private List<String> skillProficiencies;
@@ -67,47 +67,97 @@ public class Characters {
 //    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 //    private List<String> savingThrows;
 
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int armourClass;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int initiative;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int movement;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int maxHitpoints;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int currentHitPoints;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int temporaryHitPoints;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int maxHitDice;
-//
-//    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
-//    private int currentHitDice;
-//
-//    @Column(nullable = false, columnDefinition = "text")
-//    private String biography;
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int armourClass;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int initiative;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int movement;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int maxHitpoints;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int currentHitPoints;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int temporaryHitPoints;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int maxHitDice;
+
+    @Column(nullable = false, columnDefinition = ("INT(11) UNSIGNED"))
+    private int currentHitDice;
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String biography;
 
 
     public Characters() {
     }
 
     public Characters(Characters copy) {
-        name = copy.name;
-        user = copy.user;
         id = copy.id;
+        name = copy.name;
+        race = copy.race;
+        playerClass = copy.playerClass;
+        level = copy.level;
+        user = copy.user;
+        campaign = copy.campaign;
+        background = copy.background;
+        alignment = copy.alignment;
+        xp = copy.xp;
+        proficiencyBonus = copy.proficiencyBonus;
+        strength = copy.strength;
+        dexterity = copy.dexterity;
+        constitution = copy.constitution;
+        intelligence = copy.intelligence;
+        wisdom = copy.wisdom;
+        charisma = copy.charisma;
+//        skillProficiencies = copy.skillProficiencies;
+//        savingThrows = copy.savingThrows;
+        armourClass = copy.armourClass;
+        initiative = copy.initiative;
+        movement = copy.movement;
+        maxHitpoints = copy.maxHitpoints;
+        currentHitPoints = copy.currentHitPoints;
+        temporaryHitPoints = copy.temporaryHitPoints;
+        maxHitDice = copy.maxHitDice;
+        currentHitDice = copy.currentHitDice;
+        biography = copy.biography;
     }
 
-    public Characters(String name, User user, long id) {
-        this.name = name;
-        this.user = user;
+    public Characters(long id, String name, String race, String playerClass, String level, User user, Campaign campaign, String background, String alignment, long xp, int proficiencyBonus, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma, int armourClass, int initiative, int movement, int maxHitpoints, int currentHitPoints, int temporaryHitPoints, int maxHitDice, int currentHitDice, String biography) {//List<String> skillProficiencies, List<String> savingThrows,
         this.id = id;
+        this.name = name;
+        this.race = race;
+        this.playerClass = playerClass;
+        this.level = level;
+        this.user = user;
+        this.campaign = campaign;
+        this.background = background;
+        this.alignment = alignment;
+        this.xp = xp;
+        this.proficiencyBonus = proficiencyBonus;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.constitution = constitution;
+        this.intelligence = intelligence;
+        this.wisdom = wisdom;
+        this.charisma = charisma;
+//        this.skillProficiencies = skillProficiencies;
+//        this.savingThrows = savingThrows;
+        this.armourClass = armourClass;
+        this.initiative = initiative;
+        this.movement = movement;
+        this.maxHitpoints = maxHitpoints;
+        this.currentHitPoints = currentHitPoints;
+        this.temporaryHitPoints = temporaryHitPoints;
+        this.maxHitDice = maxHitDice;
+        this.currentHitDice = currentHitDice;
+        this.biography = biography;
     }
 
     public Long getId() {
@@ -132,5 +182,117 @@ public class Characters {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getPlayerClass() {
+        return playerClass;
+    }
+
+    public void setPlayerClass(String playerClass) {
+        this.playerClass = playerClass;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public Campaign getCampaign() {
+        return campaign;
+    }
+
+    public void setCampaign(Campaign campaign) {
+        this.campaign = campaign;
+    }
+
+    public String getBackground() {
+        return background;
+    }
+
+    public void setBackground(String background) {
+        this.background = background;
+    }
+
+    public String getAlignment() {
+        return alignment;
+    }
+
+    public void setAlignment(String alignment) {
+        this.alignment = alignment;
+    }
+
+    public long getXp() {
+        return xp;
+    }
+
+    public void setXp(long xp) {
+        this.xp = xp;
+    }
+
+    public int getProficiencyBonus() {
+        return proficiencyBonus;
+    }
+
+    public void setProficiencyBonus(int proficiencyBonus) {
+        this.proficiencyBonus = proficiencyBonus;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getConstitution() {
+        return constitution;
+    }
+
+    public void setConstitution(int constitution) {
+        this.constitution = constitution;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public int getCharisma() {
+        return charisma;
+    }
+
+    public void setCharisma(int charisma) {
+        this.charisma = charisma;
     }
 }
